4b0a5aaa9c255b43f6e7d86d43c531f58aeff7f8
Thaibm <thai.buiminh@ncc.asia>
2023-04-25 16:03:39 +0700

    Init lib

---
+++ src/components/Icon/Icon.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { IconProps } from './Icon.types';
+import './Icon.css';
+
+function Icon(props: IconProps) {
+  return <div className='green'>Icon</div>;
+}
+
+export default Icon;
